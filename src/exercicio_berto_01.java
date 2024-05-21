cbedce485c3f45e01d355f98a8f59521c7ee59cf
chihiroshiraishi <dev.chihiro@gmail.com>
2024-05-21 10:45:11 -0300

    commit note 2

---
+++ src/exercicio_berto_01.java
@@ -0,0 +1,19 @@
+
+public class exercicio_berto_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem\r\n"
+				+ "que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem\r\n"
+				+ "qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.\r\n"
+				+ "Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e\r\n"
+				+ "mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.\n\n"
+				+ "De R$0.00 a R$2000.00 = isento \n"
+				+ "De R$2000.01 a R$3000.00 = 8% \n"
+				+ "De R$3000.01 a R$4500.00 = 18% \n"
+				+ "Acima de R$4500.01 = 28%\n\n");
+		
+	
+	}
+
+}
